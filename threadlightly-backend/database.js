8a2c7a1dddac54a44df303561684c5d0fda9538c
Immanuella Umoren <immanuellaumoren@gmail.com>
2024-02-04 04:39:58 -0600

    deployment

--- threadlightly-backend/database.js
+++ threadlightly-backend/database.js
@@ -1,6 +1,12 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('threadlightlydatabase', 'immanuella', 'ememobong', {
-  host: 'localhost',
-  dialect: 'postgres'
+// export const sequelize = new Sequelize('threadlightlydatabase', 'immanuella', 'ememobong', {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
 });
